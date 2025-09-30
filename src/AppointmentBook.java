e2d1b60579d95bb9f98f290408e34251321b3c52
GeorgeLin <georgel115@nycstudents.net>
2025-09-30 11:47:32 -0400

    added stuff

--- src/AppointmentBook.java
+++ src/AppointmentBook.java
@@ -14,13 +14,40 @@ public class AppointmentBook
 
     public int findFreeBlock(int period, int duration)
     {
-        return 0;
+        int block = 0;
+        for(int i = 0; i < 60; i++)
+        if(isMinuteFree(period, i))
+        {
+            block++;
+            if(block == duration)
+                return i - duration + 1;
+            else block = 0;
+        }
+        return -1;
     }
 
-    public boolean makeAppointment(int startPoint, int endPeriod, int duration)
+    public boolean makeAppointment(int startPeriod, int endPeriod, int duration)
     {
+        schedule = new boolean[8][60];
+        for(int i = 25; i < 30; i++) schedule[1][i] = true;
+        for(int i = 0; i < 15; i++) schedule[1][i] = true;
+        for(int i = 41; i < 60; i++) schedule[1][i] = true;
+        for(int i = 5; i < 30; i++) schedule[1][i] = true;
+        for(int i = 44; i < 60; i++) schedule[1][i] = true;
+        for(int i = startPeriod; i <= endPeriod; i++)
+            int freeBlock = findFreeBlock(i, duration);
         return false;
     }
 
+    public void printPeriod(int period)
+    {
+        for(int i = 0; i < schedule[period - 1].length; i++)
+        System.out.println(i + " " + schedule[period - 1][i]);
+    }
 
+    private void reserveBlock(int period, int startMinute, int duration)
+    {
+      for(int i = startMinute; i < startMinute + duration; i++)
+          schedule[period - 1][i] = false;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,16 @@ public class Main
         boolean[][] schedule = new boolean[8][60];
         AppointmentBook a = new AppointmentBook(schedule);
         for(int i = 10; i < 15; i++) schedule[1][i] = true;
+        for(int i = 30; i < 44; i++) schedule[1][i] = true;
+        for(int i = 50; i < 60; i++) schedule[1][i] = true;
+        a.printPeriod(2);
+        AppointmentBook b = new AppointmentBook(schedule);
+        int period = 2;
+        while(period < 5)
+        {
+            System.out.println("Period: " + period);
+            b.printPeriod(period);
+            period++;
+        }
     }
 }
